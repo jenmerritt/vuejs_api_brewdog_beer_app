cec6bb53a64c0789f1e9df07df2735f4ec9a87d7
Jen Merritt <jenjomez@gmail.com>
2020-01-16 11:42:26 +0000

    beerlist working

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <template>
   <div id="app">
+    <beer-list :beers="beers"></beer-list>
   </div>
 </template>
 
 <script>
 
+import {eventBus} from './main.js';
+import BeerList from './components/BeerList.vue';
+
 export default {
   name: 'app',
   components: {
-
+    "beer-list": BeerList
+  },
+  data(){
+    return {
+      beers: []
+    };
+  },
+  mounted() {
+    fetch('https://api.punkapi.com/v2/beers')
+    .then(response => response.json())
+    .then(beers => this.beers = beers)
   }
 }
 </script>
@@ -18,7 +32,7 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/BeerList.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+  <section>
+    <ul>
+      <li class="beer-item" v-for="beer in beers">
+        <span>{{ beer.name }}</span>
+        <img class="beer-image" :src="beer.image_url">
+        <button>Add to Favourites</button>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'beer-list',
+  props: ['beers']
+}
+</script>
+
+<style lang="css" scoped>
+.beer-image{
+  max-height:150px;
+}
+.beer-item{
+  margin-bottom:50px;
+}
+</style>
